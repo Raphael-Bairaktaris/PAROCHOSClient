689ee743c5ddf718af8c84bf4f13f3dbf1b98929
Raphael-Bairaktaris <bairaktaris1999@gmail.com>
2024-04-02 22:35:25 +0300

    Micro changes

--- PAROCHOSClient/Routes.cs
+++ PAROCHOSClient/Routes.cs
@@ -5,30 +5,82 @@
     /// </summary>
     public class Routes
     {
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+        /// <summary>
+        /// The base route
+        /// </summary>
         public const string BaseRoute = "https://account.parochos.gr/api";
 
+        /// <summary>
+        /// The login to subscription
+        /// </summary>
         public const string LoginRoute = $"{BaseRoute}/account/loginToSubscription";
 
+        /// <summary>
+        /// The refresh token
+        /// </summary>
         public const string RefreshTokenRoute = $"{BaseRoute}/token/refresh";
 
-        public static string GetInvoiceTransmissionRoute(string? baseUrl) => $"{baseUrl}api/send";
+        /// <summary>
+        /// The invoice transmission route
+        /// </summary>
+        /// <param name="baseUrl">The base URL</param>
+        /// <returns></returns>
+        public static string GetInvoiceTransmissionRoute(string? baseUrl) => $"{baseUrl}/send";
 
-        public static string GetPollingProcessRoute(string? baseUrl) => $"{baseUrl}api/get";
+        /// <summary>
+        /// The polling process route
+        /// </summary>
+        /// <param name="baseUrl">The base URL</param>
+        /// <returns></returns>
+        public static string GetPollingProcessRoute(string? baseUrl) => $"{baseUrl}/get";
 
-        public static string GetUploadFileRequestRoute(string? baseUrl) => $"{baseUrl}api/uploadFileRequest";
+        /// <summary>
+        /// The upload file request route
+        /// </summary>
+        /// <param name="baseUrl">The base URL</param>
+        /// <returns></returns>
+        public static string GetUploadFileRequestRoute(string? baseUrl) => $"{baseUrl}/uploadFileRequest";
 
-        public static string GetFinalizeRoute(string? baseUrl) => $"{baseUrl}api/Finalize";
+        /// <summary>
+        /// The finalize route
+        /// </summary>
+        /// <param name="baseUrl">The base URL</param>
+        /// <returns></returns>
+        public static string GetFinalizeRoute(string? baseUrl) => $"{baseUrl}/Finalize";
 
-        public static string GetFilesRoute(string? baseUrl) => $"{baseUrl}api/getFiles";
+        /// <summary>
+        /// The files route
+        /// </summary>
+        /// <param name="baseUrl">The base URL</param>
+        /// <returns></returns>
+        public static string GetFilesRoute(string? baseUrl) => $"{baseUrl}/getFiles";
 
-        public static string GetPublishLogsRoute(string? baseUrl) => $"{baseUrl}api/GetPublishLogs";
+        /// <summary>
+        /// The publish logs route
+        /// </summary>
+        /// <param name="baseUrl">The base URL</param>
+        /// <returns></returns>
+        public static string GetPublishLogsRoute(string? baseUrl) => $"{baseUrl}/GetPublishLogs";
 
+        /// <summary>
+        /// The request payment route
+        /// </summary>
+        /// <param name="baseUrl">The base URL</param>
+        /// <returns></returns>
         public static string GetRequestPaymentAPIRoute(string? baseUrl) => $"{baseUrl}/RequestPayment";
 
+        /// <summary>
+        /// The submit payment route
+        /// </summary>
+        /// <param name="baseUrl">The base URL</param>
+        /// <returns></returns>
         public static string GetSubmitPaymentAPIRoute(string? baseUrl) => $"{baseUrl}/SubmitPayment";
 
+        /// <summary>
+        /// The cancel payment route
+        /// </summary>
+        /// <param name="baseUrl">The base URL</param>
+        /// <returns></returns>
         public static string GetCancelPaymentAPIRoute(string? baseUrl) => $"{baseUrl}/CancelPayment";
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
     }
 }
